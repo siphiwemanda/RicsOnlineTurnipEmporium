82bbce35db3ae05766671d2fa3fb3d5372970129
Siphiwe <siphiwe.manda@bepsystems.co.uk>
2021-05-10 08:57:02 +0100

    Refactoring test

--- RicsOnlineTurnipEmporium.Tests/CheckoutTests/ CheckoutBitcoinTests.cs
+++ RicsOnlineTurnipEmporium.Tests/CheckoutTests/ CheckoutBitcoinTests.cs
@@ -5,46 +5,49 @@ using RicsOnlineTurnipEmporium.Domain.AccountDetails;
 namespace RicsOnlineTurnipEmporium.Tests.CheckoutTests
 {
     [TestFixture]
-    public class CheckoutBitcoinTests
+    public class CheckoutBitCoinTests
     {
         [Test]
-        public void ReturnSuccessfulResultForPaymentForBitcoin()
+        public void BitCoinCheckoutIsSuccessful()
         {
             var checkout = new Checkout();
-            var payment = checkout.MakePayment(PaymentType.BitCoin, 0,  new BitCoinAccountDetails("93098DDA-3873-484A-B4A4-19C25438A71A", "31ACD47B-60D5-4FA5-9213-D7A02C55E172", "78736511-480E-4A3C-8F07-CE4BED6E0387"));
-            
+            var payment = checkout.MakePayment(PaymentType.BitCoin, 0,
+                new BitCoinAccountDetails("93098DDA-3873-484A-B4A4-19C25438A71A",
+                    "31ACD47B-60D5-4FA5-9213-D7A02C55E172", "78736511-480E-4A3C-8F07-CE4BED6E0387"));
+
             Assert.That(payment, Is.True);
         }
 
         [Test]
-        public void ReturnFailureResultForLargePayment()
+        public void BitCoinCheckoutFailsForLargePayment()
         {
             var checkout = new Checkout();
             var makePayment = checkout.MakePayment(PaymentType.BitCoin, 500,
                 new BitCoinAccountDetails("93098DDA-3873-484A-B4A4-19C25438A71A",
                     "31ACD47B-60D5-4FA5-9213-D7A02C55E172", "78736511-480E-4A3C-8F07-CE4BED6E0387"));
-            
+
             Assert.That(makePayment, Is.False);
         }
 
         [Test]
-        public void ReturnFailureResultForSamePaymentAndReceivingWallet()
+        public void BitCoinCheckoutFailsForSamePaymentAndReceivingWallet()
         {
             var checkout = new Checkout();
             var makePayment = checkout.MakePayment(PaymentType.BitCoin, 0,
                 new BitCoinAccountDetails("93098DDA-3873-484A-B4A4-19C25438A71A",
                     "93098DDA-3873-484A-B4A4-19C25438A71A", "78736511-480E-4A3C-8F07-CE4BED6E0387"));
-            
+
             Assert.That(makePayment, Is.False);
         }
+
         [Test]
-        public void ReturnFailureResultForMissingAuthentication()
+        public void BitCoinCheckoutFailsForMissingAuthentication()
         {
             var checkout = new Checkout();
             var makePayment = checkout.MakePayment(PaymentType.BitCoin, 0,
                 new BitCoinAccountDetails("93098DDA-3873-484A-B4A4-19C25438A71A",
                     "93098DDA-3873-484A-B4A4-19C25438A71A", ""));
-            
+
             Assert.That(makePayment, Is.False);
         }
     }
--- RicsOnlineTurnipEmporium.Tests/CheckoutTests/PayPalCheckoutTests.cs
+++ RicsOnlineTurnipEmporium.Tests/CheckoutTests/PayPalCheckoutTests.cs
@@ -1,29 +1,26 @@
 using NUnit.Framework;
 using RicsOnlineTurnipEmporium.Domain;
 using RicsOnlineTurnipEmporium.Domain.AccountDetails;
-using RicsOnlineTurnipEmporium.Domain.FakePaymentServers;
-
 namespace RicsOnlineTurnipEmporium.Tests.CheckoutTests
 {
     [TestFixture]
-    public class CheckoutDebitCardTests
+    public class PayPalCheckoutTests
     {
         [Test]
-        public void DebitCheckoutIsSuccessful()
+        public void PayPalCheckoutIsSuccessful()
         {
             var checkout = new Checkout();
-            var paymentResult = checkout.MakePayment(PaymentType.DebitCard, 500,
-                new DirectDebitAccountDetails("Manda", "7777777", "444"));
+            var paymentResult = checkout.MakePayment(PaymentType.PayPal, 500,
+                new PayPalAccountDetails("70BD1813-5E24-4EB5-81BC-CB25F4EB8EAD"));
             Assert.That(paymentResult, Is.True);
         }
-
         [Test]
-        public void DebitCardThrowsErrorForIncorrectPaymentType()
+        public void PayPalThrowsErrorForIncorrectPaymentType()
         {
             var checkout = new Checkout();
             Assert.That(
                 () => checkout.MakePayment(PaymentType.BitCoin, 500,
-                    new DirectDebitAccountDetails("Manda", "7777", "444")), Throws.Exception);
+                    new PayPalAccountDetails("70BD1813-5E24-4EB5-81BC-CB25F4EB8EAD")), Throws.Exception);
         }
     }
 }
